cb8fd7964b37513b37dfcc8612e2eda08da39641
programmarreza <programarreza@gmail.com>
2023-09-17 19:47:30 +0600

    setup daisyUi and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-4xl'>Vite + React</h1>
+      <button className='btn btn-primary'>btn</button>
+      
+    </>
+  )
+}
+
+export default App
